c682e92ef0b3eb2561299c90067194d90aa1d237
Jihogrammer <jiho@kakako.com>
2021-09-12 14:58:19 +0900

    initial commit

---
+++ src/test/java/jihogrammer/ps/tis/TisApplicationTests.java
@@ -0,0 +1,13 @@
+package jihogrammer.ps.tis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
